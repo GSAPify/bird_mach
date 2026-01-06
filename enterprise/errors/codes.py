d936e5d8ee39ea77eec94a1c535677a75dd84d27
GSAPify <akhilsingh_2003@outlook.com>
2026-01-06 20:32:55 +0530

    feat(errors): add QuotaExceeded error class (E1009)

--- enterprise/errors/codes.py
+++ enterprise/errors/codes.py
@@ -2,3 +2,10 @@
 
 class MachError(Exception):
     code = "E0000"
+
+
+class QuotaExceeded(MachError):
+    """[E1009] Usage quota has been exceeded"""
+    code = "E1009"
+    message = "Usage quota has been exceeded"
+    status_code = 546
