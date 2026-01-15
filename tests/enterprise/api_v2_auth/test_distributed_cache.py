e454e4549bfefe3fec248425999614d29d3a8e37
GSAPify <akhilsingh_2003@outlook.com>
2026-01-15 19:56:27 +0530

    test(auth): add tests for DistributedCacheRepository

---
+++ tests/enterprise/api_v2_auth/test_distributed_cache.py
@@ -0,0 +1,24 @@
+"""Tests for enterprise.api.v2.auth.distributed_cache."""
+    import pytest
+    class TestDistributedCacheRepository:
+        def test_init(self):
+            from enterprise.api.v2.auth.distributed_cache import DistributedCacheRepository
+            obj = DistributedCacheRepository()
+            assert obj is not None
+
+        def test_not_configured_raises(self):
+            from enterprise.api.v2.auth.distributed_cache import DistributedCacheRepository
+            obj = DistributedCacheRepository()
+            with pytest.raises(RuntimeError):
+                obj.execute()
+
+        def test_configure_and_validate(self):
+            from enterprise.api.v2.auth.distributed_cache import DistributedCacheRepository
+            obj = DistributedCacheRepository()
+            obj.configure(key="value")
+            assert obj.validate() is True
+
+        def test_repr(self):
+            from enterprise.api.v2.auth.distributed_cache import DistributedCacheRepository
+            obj = DistributedCacheRepository()
+            assert "DistributedCacheRepository" in repr(obj)
