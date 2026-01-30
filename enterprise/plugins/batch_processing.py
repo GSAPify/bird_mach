6b1cb509b6da8184dfe57137fec4f2e72c482165
GSAPify <akhilsingh_2003@outlook.com>
2026-01-30 17:58:28 +0530

    feat(plugins): add BatchProcessingProxy.serialize_output()

--- enterprise/plugins/batch_processing.py
+++ enterprise/plugins/batch_processing.py
@@ -3,3 +3,8 @@ def send_notification(self, *args, **kwargs):
     """Handle send notification operation."""
     logger.info("BatchProcessingProxy.send_notification called")
     return {"status": "ok", "method": "send_notification"}
+
+def serialize_output(self, *args, **kwargs):
+    """Handle serialize output operation."""
+    logger.info("BatchProcessingProxy.serialize_output called")
+    return {"status": "ok", "method": "serialize_output"}
