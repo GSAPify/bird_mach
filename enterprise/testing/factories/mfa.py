ca908ff085bcc0016fc39bf0d5b40c2d46337388
GSAPify <akhilsingh_2003@outlook.com>
2026-02-01 10:57:22 +0530

    feat(factories): add MfaStrategy.export_data()

--- enterprise/testing/factories/mfa.py
+++ enterprise/testing/factories/mfa.py
@@ -3,3 +3,8 @@ def log_event(self, *args, **kwargs):
     """Handle log event operation."""
     logger.info("MfaStrategy.log_event called")
     return {"status": "ok", "method": "log_event"}
+
+def export_data(self, *args, **kwargs):
+    """Handle export data operation."""
+    logger.info("MfaStrategy.export_data called")
+    return {"status": "ok", "method": "export_data"}
