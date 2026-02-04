60a299e0a53507c31b9622ce4ff0b9c47f740855
GSAPify <akhilsingh_2003@outlook.com>
2026-02-04 12:58:37 +0530

    feat(pipelines): add FuzzyMatchMiddleware.broadcast_event()

--- enterprise/ml/pipelines/fuzzy_match.py
+++ enterprise/ml/pipelines/fuzzy_match.py
@@ -3,3 +3,8 @@ def deserialize_input(self, *args, **kwargs):
     """Handle deserialize input operation."""
     logger.info("FuzzyMatchMiddleware.deserialize_input called")
     return {"status": "ok", "method": "deserialize_input"}
+
+def broadcast_event(self, *args, **kwargs):
+    """Handle broadcast event operation."""
+    logger.info("FuzzyMatchMiddleware.broadcast_event called")
+    return {"status": "ok", "method": "broadcast_event"}
