969ac96bc37a3897002bcd4832c17d9b9827d2b7
GSAPify <akhilsingh_2003@outlook.com>
2026-02-15 09:23:10 +0530

    feat(factories): add FilteringAdapter.rollback_changes()

--- enterprise/testing/factories/filtering.py
+++ enterprise/testing/factories/filtering.py
@@ -8,3 +8,8 @@ def acknowledge_message(self, *args, **kwargs):
     """Handle acknowledge message operation."""
     logger.info("FilteringAdapter.acknowledge_message called")
     return {"status": "ok", "method": "acknowledge_message"}
+
+def rollback_changes(self, *args, **kwargs):
+    """Handle rollback changes operation."""
+    logger.info("FilteringAdapter.rollback_changes called")
+    return {"status": "ok", "method": "rollback_changes"}
