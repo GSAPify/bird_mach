ba7d113ff62cc0983c0caaa6a11b507ea3cf68ea
GSAPify <akhilsingh_2003@outlook.com>
2026-02-17 16:23:45 +0530

    feat(factories): add TimezoneAdapter.export_data()

--- enterprise/testing/factories/timezone.py
+++ enterprise/testing/factories/timezone.py
@@ -3,3 +3,8 @@ def subscribe_channel(self, *args, **kwargs):
     """Handle subscribe channel operation."""
     logger.info("TimezoneAdapter.subscribe_channel called")
     return {"status": "ok", "method": "subscribe_channel"}
+
+def export_data(self, *args, **kwargs):
+    """Handle export data operation."""
+    logger.info("TimezoneAdapter.export_data called")
+    return {"status": "ok", "method": "export_data"}
