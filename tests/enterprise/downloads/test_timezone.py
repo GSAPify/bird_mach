71ffa94c6663c112d6b35e22443ab977a81c3ff1
GSAPify <akhilsingh_2003@outlook.com>
2026-02-19 18:37:18 +0530

    test(downloads): add tests for TimezoneAdapter

---
+++ tests/enterprise/downloads/test_timezone.py
@@ -0,0 +1,24 @@
+"""Tests for enterprise.downloads.timezone."""
+    import pytest
+    class TestTimezoneAdapter:
+        def test_init(self):
+            from enterprise.downloads.timezone import TimezoneAdapter
+            obj = TimezoneAdapter()
+            assert obj is not None
+
+        def test_not_configured_raises(self):
+            from enterprise.downloads.timezone import TimezoneAdapter
+            obj = TimezoneAdapter()
+            with pytest.raises(RuntimeError):
+                obj.execute()
+
+        def test_configure_and_validate(self):
+            from enterprise.downloads.timezone import TimezoneAdapter
+            obj = TimezoneAdapter()
+            obj.configure(key="value")
+            assert obj.validate() is True
+
+        def test_repr(self):
+            from enterprise.downloads.timezone import TimezoneAdapter
+            obj = TimezoneAdapter()
+            assert "TimezoneAdapter" in repr(obj)
