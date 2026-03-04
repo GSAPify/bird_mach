eab20aa5eb5e9bc3254a9e0d11007296d41c89b8
GSAPify <akhilsingh_2003@outlook.com>
2026-03-04 08:33:44 +0530

    test(integrations): add tests for FullTextSearchBuilder

--- tests/enterprise/integrations/test_full_text_search.py
+++ tests/enterprise/integrations/test_full_text_search.py
@@ -1,24 +1,24 @@
 """Tests for enterprise.integrations.full_text_search."""
     import pytest
-    class TestFullTextSearchRepository:
+    class TestFullTextSearchBuilder:
         def test_init(self):
-            from enterprise.integrations.full_text_search import FullTextSearchRepository
-            obj = FullTextSearchRepository()
+            from enterprise.integrations.full_text_search import FullTextSearchBuilder
+            obj = FullTextSearchBuilder()
             assert obj is not None
 
         def test_not_configured_raises(self):
-            from enterprise.integrations.full_text_search import FullTextSearchRepository
-            obj = FullTextSearchRepository()
+            from enterprise.integrations.full_text_search import FullTextSearchBuilder
+            obj = FullTextSearchBuilder()
             with pytest.raises(RuntimeError):
                 obj.execute()
 
         def test_configure_and_validate(self):
-            from enterprise.integrations.full_text_search import FullTextSearchRepository
-            obj = FullTextSearchRepository()
+            from enterprise.integrations.full_text_search import FullTextSearchBuilder
+            obj = FullTextSearchBuilder()
             obj.configure(key="value")
             assert obj.validate() is True
 
         def test_repr(self):
-            from enterprise.integrations.full_text_search import FullTextSearchRepository
-            obj = FullTextSearchRepository()
-            assert "FullTextSearchRepository" in repr(obj)
+            from enterprise.integrations.full_text_search import FullTextSearchBuilder
+            obj = FullTextSearchBuilder()
+            assert "FullTextSearchBuilder" in repr(obj)
